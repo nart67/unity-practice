853ee0cc80a22f7f2dcc292d6d6292b7a94e0f62
Dennis Tran <nartdensin@gmail.com>
2018-03-11 21:03:16 -0700

    initial set up, implement mouse movement of paddle

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+    public LevelManager levelManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("trigger");
+        levelManager.LoadLevel("Win");
+    }
+
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y);
+        float mousePosInBlocks = Mathf.Clamp( Input.mousePosition.x / Screen.width * 16, 0.5f, 15.5f);
+
+        paddlePos.x = mousePosInBlocks;
+
+        this.transform.position = paddlePos;
+	}
+}
