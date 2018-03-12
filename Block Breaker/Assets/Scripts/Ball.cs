790532718f5c6ed991b73334b253fdf6ca405721
Dennis Tran <nartdensin@gmail.com>
2018-03-11 21:39:04 -0700

    set up ball movement and walls

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+    private Vector3 paddleToBallVector;
+    private bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+        print(paddleToBallVector.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Lock ball to paddle
+        if (!hasStarted) this.transform.position = paddle.transform.position + paddleToBallVector;
+
+        // Wait for mouse press to launch
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Mouse clicked, launch ball");
+            hasStarted = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+        }
+
+	}
+}
