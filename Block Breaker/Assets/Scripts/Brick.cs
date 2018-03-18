834660e37adae1f99e6025b6e37245d62e3c2d9b
Dennis Tran <nartdensin@gmail.com>
2018-03-17 20:19:56 -0700

    implement block breaking

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
+    private LevelManager levelManager;
     public int maxHits;
     private int timesHit;
+    public Sprite[] hitSprites;
 
 	// Use this for initialization
 	void Start () {
-        timesHit = 0;	
+        timesHit = 0;
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +23,24 @@ public class Brick : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         timesHit++;
-        print(timesHit);
+        if (timesHit >= maxHits)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            LoadSprites();
+        }
+    }
+
+    private void Win()
+    {
+        levelManager.LoadNextLevel();
+    }
+
+    private void LoadSprites()
+    {
+        int spriteIndex = timesHit - 1;
+        this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 }
