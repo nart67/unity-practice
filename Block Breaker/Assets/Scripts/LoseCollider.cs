b233f24359b848d8212317f7a9cffbfde9dfb938
Dennis Tran <nartdensin@gmail.com>
2018-03-17 20:17:26 -0700

    load proper lose scene

--- Block Breaker/Assets/Scripts/LoseCollider.cs
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -13,7 +13,7 @@ public class LoseCollider : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("trigger");
-        levelManager.LoadLevel("Win");
+        levelManager.LoadLevel("Lose");
     }
 
 }
