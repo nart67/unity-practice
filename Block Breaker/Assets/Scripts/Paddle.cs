3bf056ded40908dc9bb4e770f2405bfe9a08ca0b
Dennis Tran <nartdensin@gmail.com>
2018-03-17 21:55:45 -0700

    add autoplay

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
+    public bool autoPlay = false;
+    private Ball ball;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        ball = GameObject.FindObjectOfType<Ball>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if (!autoPlay)
+        {
+            MoveWithMouse();
+        }
+        else
+        {
+            AutoPlay();
+        }
+	}
+
+    void MoveWithMouse()
+    {
         Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y);
-        float mousePosInBlocks = Mathf.Clamp( Input.mousePosition.x / Screen.width * 16, 0.5f, 15.5f);
+        float mousePosInBlocks = Mathf.Clamp(Input.mousePosition.x / Screen.width * 16, 0.5f, 15.5f);
 
         paddlePos.x = mousePosInBlocks;
 
         this.transform.position = paddlePos;
-	}
+    }
+
+    void AutoPlay()
+    {
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y);
+        Vector3 ballPos = ball.transform.position;
+    
+        paddlePos.x = Mathf.Clamp(ballPos.x, 0.5f, 15.5f);
+
+        this.transform.position = paddlePos;
+    }
 }
