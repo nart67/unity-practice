cb4d2b787caa86ab2406b48eb1215f37bd20bf9a
Dennis Tran <nartdensin@gmail.com>
2018-03-21 22:36:11 -0700

    allow player ship to shoot lasers

--- Laser Defender/Assets/Entities/Player/PlayerController.cs
+++ Laser Defender/Assets/Entities/Player/PlayerController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public GameObject laser;
 
     public float speed = 15.0f;
     public float padding = 1f;
+    public float projectileSpeed;
+    public float fireRate;
 
     float xmin = -5;
     float xmax = 5;
@@ -25,12 +28,26 @@ public class PlayerController : MonoBehaviour {
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("Fire", 0.000001f, fireRate);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("Fire");
         }
 
         float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+
+    private void Fire()
+    {
+        GameObject beam = Instantiate(laser, transform.position, Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
     }
 }
