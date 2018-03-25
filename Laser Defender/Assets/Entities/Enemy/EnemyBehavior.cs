c48fb16e396588333f8062a57b6f93db688669ab
Dennis Tran <nartdensin@gmail.com>
2018-03-25 14:13:38 -0700

    add sounds, respawn, and animations

--- Laser Defender/Assets/Entities/Enemy/EnemyBehavior.cs
+++ Laser Defender/Assets/Entities/Enemy/EnemyBehavior.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyBehavior : MonoBehaviour {
     public float health = 150;
     public float shotsPerSecond = 0.5f;
     public GameObject projectile;
     public float projectileSpeed = 10f;
+    public AudioClip fireSound;
+    public AudioClip deathSound;
+
+    public int scoreValue = 150;
+    private ScoreKeeper scoreKeeper;
 
 	// Use this for initialization
 	void Start () {
-		
+        scoreKeeper = GameObject.Find("Score").GetComponent<ScoreKeeper>();
 	}
 	
 	// Update is called once per frame
@@ -30,6 +36,8 @@ public class EnemyBehavior : MonoBehaviour {
             health -= projectile.GetDamage();
             if (health <= 0)
             {
+                scoreKeeper.Score(scoreValue);
+                AudioSource.PlayClipAtPoint(deathSound, transform.position);
                 Destroy(gameObject);
             }
             projectile.Hit();
@@ -38,8 +46,8 @@ public class EnemyBehavior : MonoBehaviour {
 
     void Fire()
     {
-        Vector3 startPosition = transform.position + new Vector3(0, -1, 0);
-        GameObject laser = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
+        GameObject laser = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
         laser.GetComponent<Rigidbody2D>().velocity = Vector2.down * projectileSpeed;
+        AudioSource.PlayClipAtPoint(fireSound, transform.position);
     }
 }
--- Laser Defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Laser Defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -8,17 +8,14 @@ public class EnemySpawner : MonoBehaviour {
     public float height = 5f;
     private bool movingRight = true;
     public float speed = 5f;
+    public float spawnDelay = 0.5f;
 
     private float xmin;
     private float xmax;
 
     // Use this for initialization
     void Start () {
-        foreach (Transform child in transform)
-        {
-            GameObject enemy = Instantiate(enemyPrefab, child.position, Quaternion.identity) as GameObject;
-            enemy.transform.parent = child;
-        }
+        SpawnEnemies();
 
         float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
         Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
@@ -49,7 +46,51 @@ public class EnemySpawner : MonoBehaviour {
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
 
+        if (AllMembersDead())
+        {
+            SpawnUntilFull();
+        }
+    }
 
+    private Transform NextFreePosition()
+    {
+        foreach (Transform childPositionGameObject in transform)
+        {
+            if (childPositionGameObject.childCount == 0) return childPositionGameObject.transform;
+        }
+        return null;
+    }
+
+    private bool AllMembersDead()
+    {
+        foreach(Transform childPositionGameObject in transform)
+        {
+            if (childPositionGameObject.childCount > 0) return false;
+        }
+        return true;
+    }
+
+    private void SpawnEnemies()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+    }
+
+    private void SpawnUntilFull()
+    {
+        Transform freePosition = NextFreePosition();
+        if (freePosition)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, freePosition.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = freePosition;
+        }
+        if (NextFreePosition())
+        {
+            Invoke("SpawnUntilFull", spawnDelay);
+        }
     }
 
     public void OnDrawGizmos()
