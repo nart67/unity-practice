a417942381a58ccf861b34672958f208c0d60240
Dennis Tran <nartdensin@gmail.com>
2018-03-24 22:01:59 -0700

    enemies can get destroyed and fire back

---
+++ Laser Defender/Assets/Entities/Enemy/EnemyBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+    public float health = 150;
+    public float shotsPerSecond = 0.5f;
+    public GameObject projectile;
+    public float projectileSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float probability = Time.deltaTime * shotsPerSecond;
+        if (Random.value < probability)
+        {
+            Fire();
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Projectile projectile = collision.gameObject.GetComponent<Projectile>();
+        if (projectile)
+        {
+            health -= projectile.GetDamage();
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+            projectile.Hit();
+        }
+    }
+
+    void Fire()
+    {
+        Vector3 startPosition = transform.position + new Vector3(0, -1, 0);
+        GameObject laser = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = Vector2.down * projectileSpeed;
+    }
+}
